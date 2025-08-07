ece0f6ddbbe28b1a7c974b1a51210a25631ff3a9
boyou.chen <boyou.chen@hnsquare.com.tw>
2025-08-07 15:40:48 +0800

    feat: 初始專案

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+export const CartContext = createContext({});
+export const initialState = {
+  cartList: [],
+};
+export const reducer = (state, action) => {
+    
+}
